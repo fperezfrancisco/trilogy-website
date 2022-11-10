5047317f858369e7f0500b7cf9c6f4055a5e2566
Francisco Perez <fperezfrancisco4@gmail.com>
2022-11-10 10:29:44 -0800

    first commit again

---
+++ script.js
@@ -0,0 +1,12 @@
+/** identify buttons */
+
+const signUpBtn = document.querySelector(".sign-up-btn");
+const conntactBtn = document.querySelector(".contact-btn");
+const learnMoreBtn_about = document.querySelector("#learn-more");
+// const learnMoreBtn_offer = ;
+
+signUpBtn.addEventListener("onclick", () => {
+  window.open(
+    "https://docs.google.com/forms/d/e/1FAIpQLScQfb0lk0B9FIbXJKTSwI1MmadfXSiUIC3fQJfVg0D_K-BGgw/viewform?usp=pp_url"
+  );
+});
