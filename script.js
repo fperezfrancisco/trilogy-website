c23b4a6304efabf96af4f1842367a05ebba0e596
francisco <fperezfrancisco4@gmail.com>
2023-01-05 21:31:15 -0800

    finished all animations

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function reveal() {
   for (var i = 0; i < reveals.length; i++) {
     var windowHeight = window.innerHeight;
     var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 175;
+    var elementVisible = 100;
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
